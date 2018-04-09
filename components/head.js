bc5aa9504122bfc510bbca1fe627181f3ca5a008
jhta <jeisonhs93@gmail.com>
2018-04-09 11:42:51 -0500

    basic implemntation

--- components/head.js
+++ components/head.js
@@ -6,25 +6,34 @@ const defaultOGURL = ''
 const defaultOGImage = ''
 
 const Head = (props) => (
-  <NextHead>
-    <meta charset="UTF-8" />
-    <title>{props.title || ''}</title>
-    <meta name="description" content={props.description || defaultDescription} />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="icon" sizes="192x192" href="/static/touch-icon.png" />
-    <link rel="apple-touch-icon" href="/static/touch-icon.png" />
-    <link rel="mask-icon" href="/static/favicon-mask.svg" color="#49B882" />
-    <link rel="icon" href="/static/favicon.ico" />
-    <meta property="og:url" content={props.url || defaultOGURL} />
-    <meta property="og:title" content={props.title || ''} />
-    <meta property="og:description" content={props.description || defaultDescription} />
-    <meta name="twitter:site" content={props.url || defaultOGURL} />
-    <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:image" content={props.ogImage || defaultOGImage} />
-    <meta property="og:image" content={props.ogImage || defaultOGImage} />
-    <meta property="og:image:width" content="1200" />
-    <meta property="og:image:height" content="630" />
-  </NextHead>
+  <div>
+    <NextHead>
+      <meta charset="UTF-8" />
+      <title>{props.title || ''}</title>
+      <meta name="description" content={props.description || defaultDescription} />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" sizes="192x192" href="/static/touch-icon.png" />
+      <link rel="apple-touch-icon" href="/static/touch-icon.png" />
+      <link rel="mask-icon" href="/static/favicon-mask.svg" color="#49B882" />
+      <link rel="icon" href="/static/favicon.ico" />
+      <meta property="og:url" content={props.url || defaultOGURL} />
+      <meta property="og:title" content={props.title || ''} />
+      <meta property="og:description" content={props.description || defaultDescription} />
+      <meta name="twitter:site" content={props.url || defaultOGURL} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:image" content={props.ogImage || defaultOGImage} />
+      <meta property="og:image" content={props.ogImage || defaultOGImage} />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.2/css/bulma.min.css" />
+    </NextHead>
+    <style jsx-global>{
+      `
+        * { box-sizing: border-box; margin: 0; padding: 0  }
+      `
+    }
+    </style>
+  </div>
 )
 
 Head.propTypes = {
